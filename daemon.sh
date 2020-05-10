cc9f35a026ee57cb764d13f8d15cf1c310c71e41
naripok <fernando.canteruccio@gmail.com>
2020-05-10 01:35:55 -0300

    better pid values

--- daemon.sh
+++ daemon.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-let leak=2
-let p=30
+function clearExit () {
+  echo "2" > /sys/class/drm/card0/device/hwmon/hwmon3/pwm1_enable
+  echo Setting fan to automatic control mode.
+}
+trap clearExit EXIT
+
+let alpha=2
+let p=42
 let i=20
 
 let acc=0
 let delta=0
 
+echo Setting fan to manual control mode.
+echo "1" > /sys/class/drm/card0/device/hwmon/hwmon3/pwm1_enable
+
 echo Running...
 
 while true; do
-  let junction=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/temp2_input)
+  let junction=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/temp1_input)
   let state=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/pwm1)
-  let fan=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/fan1_input)
 
   let temp=$junction/1000
 
-  let target=($temp-50)*255/60
+  let target=($temp-50)*255/40
   if [ $target -lt 0 ]; then target=0; fi
 
   let delta=$target-$state
 
-  let acc=$acc/$leak
-  let acc=$acc+$delta*2
+  let acc=$acc/$alpha+$delta*$alpha
 
   let pwm_target=$state+$delta/$p+$acc/$i
 
@@ -30,6 +37,7 @@ while true; do
   if [ $pwm_target -lt 0 ]; then pwm_target=0; fi
 
   if [ $FANDEBUG ]; then
+    let fan=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/fan1_input)
     echo ================
     echo temp: $temp
     echo state: $state
@@ -42,5 +50,6 @@ while true; do
 
   echo $pwm_target > /sys/class/drm/card0/device/hwmon/hwmon3/pwm1
 
-  sleep 0.2
+  sleep 0.2 &
+  wait
 done
