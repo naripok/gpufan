15fabb02f5c0ff39acf2edf9dfedc393ad0ed731
naripok <fernando.canteruccio@gmail.com>
2020-05-10 11:24:40 -0300

    start after display-manager, init conditions check

--- daemon.sh
+++ daemon.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
+DEVICE=/sys/class/drm/card0/device/hwmon/hwmon3
+
+# check if device exists and is amdgpu
+if [ ! -d "$DEVICE" ]; then
+  echo Device not found. Exiting.
+  exit 1
+fi
+
+if [ $(cat $DEVICE/name) != 'amdgpu' ]; then
+  echo Device is not amdgpu. Exiting
+  exit 1
+fi
+
+# cleanup function
 function clearExit () {
-  echo "2" > /sys/class/drm/card0/device/hwmon/hwmon3/pwm1_enable
+  echo "2" > $DEVICE/pwm1_enable
   echo Setting fan to automatic control mode.
 }
 trap clearExit EXIT
 
+# leaky pid params
 let alpha=2
 let p=46
 let i=20
 
+# init vars
 let acc=0
 let delta=0
 
 echo Setting fan to manual control mode.
-echo "1" > /sys/class/drm/card0/device/hwmon/hwmon3/pwm1_enable
+echo "1" > $DEVICE/pwm1_enable
 
 echo Running...
 
 while true; do
-  let junction=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/temp1_input)
-  let state=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/pwm1)
+  let junction=$(cat $DEVICE/temp1_input)
+  let state=$(cat $DEVICE/pwm1)
 
   let temp=$junction/1000
 
@@ -37,7 +53,7 @@ while true; do
   if [ $pwm_target -lt 0 ]; then pwm_target=0; fi
 
   if [ $FANDEBUG ]; then
-    let fan=$(cat /sys/class/drm/card0/device/hwmon/hwmon3/fan1_input)
+    let fan=$(cat $DEVICE/fan1_input)
     echo ================
     echo temp: $temp
     echo state: $state
@@ -48,7 +64,7 @@ while true; do
     echo fan: $fan
   fi
 
-  echo $pwm_target > /sys/class/drm/card0/device/hwmon/hwmon3/pwm1
+  echo $pwm_target > $DEVICE/pwm1
 
   sleep 0.2 &
   wait
